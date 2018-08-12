ba4114013e52789de1e051ee38d71c2aeccb7898
reliabilitylogdata <reliabilitylogdata@github.com>
2018-08-12 17:14:15 -0300

    main

--- src/main.cu
+++ src/main.cu
@@ -1,16 +1,14 @@
-
 #include "GEMM.cuh"
 #include <iostream>
 #include <cuda_fp16.h>
 
-
-template<class T> void print(T* c, size_t n){
-		for(size_t i = 0; i < n; i++){
-			for(size_t j = 0; j < n; j++){
-				std::cout << c[i * n + j] << " ";
-			}
-			std::cout << std::endl;
+template<class T> void print(T* c, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = 0; j < n; j++) {
+			std::cout << __half2float(c[i * n + j]) << " ";
 		}
+		std::cout << std::endl;
+	}
 }
 
 template<class T> void matrix_mul(size_t siz, size_t n) {
@@ -24,25 +22,26 @@ template<class T> void matrix_mul(size_t siz, size_t n) {
 	radiation::GEMM<T> test(a, b, c, n, n, n);
 	test.mul();
 	test.pull_array(c);
-	//print(c);
+	print(c, n);
 	free(a);
 	free(b);
 	free(c);
 }
 
-int main() {
-	size_t n = 2048;
-	size_t siz = n * n;
+int main(int argc, char** argv) {
+	if (argc > 2) {
+		size_t n = atoi(argv[1]);
+		size_t siz = n * n;
 
-	std::cout << "Multiplying for double" << std::endl;
-	matrix_mul<double>(siz, n);
+		std::cout << "Multiplying for double" << std::endl;
+		matrix_mul<double>(siz, n);
 
-	std::cout << "Multiplying for float" << std::endl;
-	matrix_mul<float>(siz, n);
+		std::cout << "Multiplying for float" << std::endl;
+		matrix_mul<float>(siz, n);
 
-	std::cout << "Multiplying for half" << std::endl;
-	matrix_mul<half>(siz, n);
-
+		std::cout << "Multiplying for half" << std::endl;
+		matrix_mul<half>(siz, n);
+	}
 
 	return 0;
 }
